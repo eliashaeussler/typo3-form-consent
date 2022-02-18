4d93c2ea87c2a2583bb544f1a279e4b8d547c6a5
Elias Häußler <e.haeussler@familie-redlich.de>
2022-02-18 17:07:16 +0100

    [TASK] Set header in all PHP files using PHP-CS-Fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,7 +2,14 @@
 
 declare(strict_types=1);
 
+$header = <<<EOM
+This file is part of the TYPO3 CMS extension "form_consent".
+
+Copyright (C) %d Elias Häußler <elias@haeussler.dev>
+EOM;
+
 $config = \TYPO3\CodingStandards\CsFixerConfig::create()
+    ->setHeader(sprintf($header, \date('Y')))
     ->addRules([
         'native_function_invocation' => true,
     ]);
--- Tests/Functional/Domain/Repository/ConsentRepositoryTest.php
+++ Tests/Functional/Domain/Repository/ConsentRepositoryTest.php
@@ -5,20 +5,16 @@ declare(strict_types=1);
 /*
  * This file is part of the TYPO3 CMS extension "form_consent".
  *
- * Copyright (C) 2020 Elias Häußler <elias@haeussler.dev>
+ * Copyright (C) 2022 Elias Häußler <elias@haeussler.dev>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * The TYPO3 project - inspiring people to share!
  */
 
 namespace EliasHaeussler\Typo3FormConsent\Tests\Functional\Domain\Repository;
