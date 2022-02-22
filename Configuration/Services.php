25c90e0524cac73b480634bf256017c442754967
Elias Häußler <e.haeussler@familie-redlich.de>
2022-02-22 10:42:16 +0100

    [TASK] Use custom license header in PHP files

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -7,14 +7,18 @@ declare(strict_types=1);
  *
  * Copyright (C) 2022 Elias Häußler <elias@haeussler.dev>
  *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
  *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * The TYPO3 project - inspiring people to share!
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 namespace EliasHaeussler\Typo3FormConsent;
