e87141747a187d9674aff0b7527c86c95109826d
Elias Häußler <elias@haeussler.dev>
2025-07-16 11:15:49 +0200

    [BUGFIX] Disable third-party hooks when invoking finishers
    Resolves: #180

--- Tests/Acceptance/Frontend/Controller/ConsentControllerCest.php
+++ Tests/Acceptance/Frontend/Controller/ConsentControllerCest.php
@@ -56,7 +56,7 @@ final class ConsentControllerCest
         $I->seeInDatabase(
             Src\Domain\Model\Consent::TABLE_NAME,
             [
-                'data' => '{"email-1":"user@example.com","fileupload-1":null}',
+                'data like' => '{"cr-field":"%","email-1":"user@example.com","fileupload-1":null}',
                 'email' => 'user@example.com',
                 'state' => Src\Enums\ConsentState::Approved->value,
                 'form_persistence_identifier' => '1:/form_definitions/contact.form.yaml',
@@ -234,6 +234,17 @@ final class ConsentControllerCest
 
         $I->click('Dismiss');
         $I->see('Consent successfully revoked');
+    }
+
+    public function canApproveConsentWithThirdPartyHooksInstalled(Tests\Acceptance\Support\AcceptanceTester $I): void
+    {
+        $I->writeConfiguration('form_crshield', 'crJavaScriptDelay', '5');
+
+        $this->submitFormAndExtractUrls($I, Tests\Acceptance\Support\Helper\Form::CONFIRMATION_AFTER_APPROVE);
+
+        $I->amOnPage($this->approveUrl);
+
+        $I->see('Thanks for your consent.');
     }
 
     public function canPreviewValidationPluginWithActiveBackendSession(Tests\Acceptance\Support\AcceptanceTester $I): void
